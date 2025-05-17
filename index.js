75a398e9e11a5300809ba5889da53e50197b0021
bency-fathima <biyyubencyali@gmail.com>
2025-05-17 15:55:05 +0530

    new

--- index.js
+++ index.js
@@ -266,5 +266,5 @@ app.post("/pdf", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3994;
+const PORT =  5663;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
